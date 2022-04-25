4d20469d2fcc57a9e6bf133f53575e28e5d3f2b5
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2022-04-25 09:06:11 +0100

    use icons in currentWeather section

--- src/currentWeather.js
+++ src/currentWeather.js
@@ -4,6 +4,15 @@ import thunderstormIcon from "./assets/icons/thunderstorm.svg";
 import rainIcon from "./assets/icons/rain.svg";
 import snowIcon from "./assets/icons/snow.svg";
 import fogIcon from "./assets/icons/fog.svg";
+import sunriseIcon from "./assets/icons/sunrise.svg";
+import sunsetIcon from "./assets/icons/sunset.svg";
+import gaugeIcon from "./assets/icons/gauge.svg";
+import windIcon from "./assets/icons/wind.svg";
+import compassIcon from "./assets/icons/compass.svg";
+import thermometerLow from "./assets/icons/thermometerLow.svg";
+import thermometerHigh from "./assets/icons/thermometerHigh.svg";
+import thermometerCheck from "./assets/icons/thermometerCheck.svg";
+
 import pubsub from "./pubsub";
 
 const currentWeather = (() => {
@@ -13,29 +22,42 @@ const currentWeather = (() => {
 
     container.innerHTML = `
       <h1 id="city-name" class="city-name"></h1>
-      <h2  class="current-temp"><span id="current-temp"></span><span class="degree-symbol">˚C</span></h2>
-      <div class="container-tl info-box">
-        <p>Feels like: <span id="feels-like"></span><span class="degree-symbol">˚C</span></p>
-        <p>Low: <span id="min-temp"></span><span class="degree-symbol">˚C</span></p>
-        <p>High: <span id="max-temp"></span><span class="degree-symbol">˚C</span></p>
+      <h2 class="current-temp"><span id="current-temp"></span><span class="degree-symbol">˚C</span></h2>
+      <div class="high-low">
+        <p class="row-center"><img src="${thermometerLow}" alt="wind speed icon" class="weather-icon small"><span id="min-temp"></span><span class="degree-symbol">˚C</span></p>
+        <p class="row-center"><img src="${thermometerHigh}" alt="wind speed icon" class="weather-icon small"><span id="max-temp"></span><span class="degree-symbol">˚C</span></p>
       </div>
-      <div class="container-tr info-box">
+      <div class="container-left info-box">
+        <p class="row-center"><img src="${thermometerCheck}" alt="feels like icon" class="weather-icon small"><span id="feels-like"></span><span class="degree-symbol">˚C</span></p>
+        <p class="row-center"><img src="${windIcon}" alt="wind speed icon" class="weather-icon small"><span id="wind-speed"></span>mph</p>
+        <p class="row-center"><img src="${compassIcon}" alt="wind direction icon" class="weather-icon small"><span id="wind-direction"></span></p>
+      </div>
+      <div class="container-middle info-box">
         <img src="" alt="" id="weather-icon" class="weather-icon center">
         <p id="weather-description" class="center"></p>
       </div>
-      <div class="container-bl info-box">
-        <p>Wind speed: <span id="wind-speed"></span>mph</p>
-        <p>Direction: <span id="wind-direction"></span>deg</p>
-      </div>
-      <div class="container-br info-box">
-        <p>Sunrise: <span id="sunrise"></span></p>
-        <p>Sunset: <span id="sunset"></span></p>
-        <p >Pressure: <span id="pressure"></span>hPa</p>
+      <div class="container-right info-box">
+        <p class="row-center"><img src="${sunriseIcon}" alt="sunrise icon" class="weather-icon small"><span id="sunrise"></span></p>
+        <p class="row-center"><img src="${sunsetIcon}" alt="sunset icon" class="weather-icon small"><span id="sunset"></span></p>
+        <p class="row-center"><img src="${gaugeIcon}" alt="pressure icon" class="weather-icon small"><span id="pressure"></span>hPa</p>
       </div>
     `;
 
     return container;
   })();
+
+  const getDirection = (degrees) => {
+    if ((degrees >= 337.5 && degrees <= 360) || (degrees >= 0 && degrees < 22.5)) return "North";
+    if (degrees >= 22.5 && degrees < 67.5) return "North East";
+    if (degrees >= 67.5 && degrees < 112.5) return "East";
+    if (degrees >= 112.5 && degrees < 157.5) return "South East";
+    if (degrees >= 157.5 && degrees < 202.5) return "South";
+    if (degrees >= 202.5 && degrees < 247.5) return "South West";
+    if (degrees >= 247.5 && degrees < 292.5) return "West";
+    if (degrees >= 292.5 && degrees < 337.5) return "North West";
+
+    return "Unknown";
+  };
 
   const displayFunctions = [
     ({ cityName }) => { document.getElementById("city-name").textContent = cityName; },
@@ -61,7 +83,7 @@ const currentWeather = (() => {
     ({ sunset }) => { document.getElementById("sunset").textContent = sunset; },
     ({ pressure }) => { document.getElementById("pressure").textContent = pressure; },
     ({ windSpeed }) => { document.getElementById("wind-speed").textContent = windSpeed; },
-    ({ windDeg }) => { document.getElementById("wind-direction").textContent = windDeg; },
+    ({ windDeg }) => { document.getElementById("wind-direction").textContent = getDirection(windDeg); },
   ];
 
   const displayCurrentData = (data) => displayFunctions.forEach((fn) => fn(data));
